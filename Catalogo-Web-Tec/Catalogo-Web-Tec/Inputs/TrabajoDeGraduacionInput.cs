3d8d4639676f63a2c92395d6a14183fef451538d
Joseline-Pineda <65983491+Joseline-Pineda@users.noreply.github.com>
2022-05-14 08:53:16 -0600

    Agregando Inputs

---
+++ Catalogo-Web-Tec/Catalogo-Web-Tec/Inputs/TrabajoDeGraduacionInput.cs
@@ -0,0 +1,16 @@
+﻿using CatalogoWebApp.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace CatalogoWebApp.Inputs
+{
+    public class TrabajoDeGraduacionInput
+    {
+        public string Titulo { get; set; }
+        public string AutorId { get; set; }
+        public string Descripcion { get; set; }
+        public IFormFile Imagen { get; set; }
+        public IFormFile File { get; set; }
+        public int Year { get; set; }
+        public string TipoId { get; set; }
+    }
+}
