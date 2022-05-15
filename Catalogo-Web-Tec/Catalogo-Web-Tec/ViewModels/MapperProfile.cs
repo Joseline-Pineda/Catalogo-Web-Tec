86e62f32f189490c0deaed5692c6fbbdf13f30b1
IsaiGarcia94 <93231501+IsaiGarcia94@users.noreply.github.com>
2022-05-14 18:04:46 -0600

    Add files via upload

---
+++ Catalogo-Web-Tec/Catalogo-Web-Tec/ViewModels/MapperProfile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using AutoMapper;
+using CatalogoWebApp.Inputs;
+using CatalogoWebApp.Models;
+
+namespace CatalogoWebApp.ViewModels
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Autor, AutorViewModel>()
+                .ForMember(vm=> vm.NombreCompleto,opt=> opt.MapFrom(src=>$"{src.Nombres} {src.Apellidos}"))
+                .ForMember(vm=> vm.Info,opt=> opt.MapFrom(src=>$"{src.Codigo}-{src.Nombres} {src.Apellidos}"));
+
+            CreateMap<TrabajoDeGraduacion, TrabajoDeGraduacionInput>()
+                .ReverseMap()
+                .ForMember(i=>i.Fecha,opt=> opt.MapFrom(src=>new DateTime(src.Year,1,1)))
+                .ForMember(i=>i.PathImagen, opt =>
+                {
+                    opt.PreCondition(x=>x.Imagen==null);
+                    opt.UseDestinationValue();
+                })
+                .ForMember(i=>i.PathFile, opt =>
+                {
+                    opt.PreCondition(x=>x.File==null);
+                    opt.UseDestinationValue();
+                });
+
+
+            CreateMap<Models.NoSQL.Autor, AutorViewModel>()
+                .ForMember(vm=>vm.AutorId,opt=>opt.MapFrom(src=>src.Id))
+                .ForMember(vm=> vm.NombreCompleto,opt=> opt.MapFrom(src=>$"{src.Nombres} {src.Apellidos}"))
+                .ForMember(vm=> vm.Info,opt=> opt.MapFrom(src=>$"{src.Codigo}-{src.Nombres} {src.Apellidos}"));
+
+            CreateMap<Models.NoSQL.TrabajoDeGraduacion, TrabajoDeGraduacionInput>()
+                .ReverseMap()
+                .ForMember(i=>i.Fecha,opt=> opt.MapFrom(src=>new DateTime(src.Year,1,1)))
+                .ForMember(i=>i.PathImagen, opt =>
+                {
+                    opt.PreCondition(x=>x.Imagen==null);
+                    opt.UseDestinationValue();
+                })
+                .ForMember(i=>i.PathFile, opt =>
+                {
+                    opt.PreCondition(x=>x.File==null);
+                    opt.UseDestinationValue();
+                });
+        }
+    }
+}
